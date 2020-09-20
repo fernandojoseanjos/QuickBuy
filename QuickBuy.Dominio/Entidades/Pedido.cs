47f59e9e7bf915afc0ffe84503cb00d38dc6c3ea
fernandojoseanjos <fernandoanjos0202@gmail.com>
2020-09-20 01:39:10 -0300

    Adicionei as  entidades e forma de pagamento.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string Cep { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string NumeroEndereco { get; set; }
+        public List<ItemPedido> ItensPedido { get; set; }
+
+    }
+}
